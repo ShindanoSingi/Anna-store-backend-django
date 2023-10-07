d4024f7990a599989b73657069d0812896c59e78
Shindano Singi <shindanosingi@sekur.com>
2023-10-07 12:09:53 -0400

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Nav, NavDropdown, Navbar, Container, Row } from "react-bootstrap";
+
+function Header() {
+      return (
+            <header>
+                  <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+                        <Container>
+                              <Navbar.Brand href="/">
+								AnnaShop
+                              </Navbar.Brand>
+                              <Navbar.Toggle aria-controls="navbarScroll" />
+                              <Navbar.Collapse id="navbarScroll">
+                                    <Nav className="mr-auto">
+                                          <Nav.Link href="/cart">
+											<i className="fas fa-shopping-cart"></i>
+                                                Cart
+                                          </Nav.Link>
+                                          <Nav.Link href="/login">
+											<i className="fas fa-user"></i>
+                                                Login
+                                          </Nav.Link>
+                                    </Nav>
+                              </Navbar.Collapse>
+                        </Container>
+                  </Navbar>
+            </header>
+      );
+}
+
+export default Header;
