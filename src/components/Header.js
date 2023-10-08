41394af8d9c8f38cf08b5d495996cd71cc8f62ba
Shindano Singi <shindanosingi@sekur.com>
2023-10-08 12:17:21 -0400

    Partially cddone weith the frontend.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,49 @@
 import React from "react";
-import { Nav, NavDropdown, Navbar, Container, Row } from "react-bootstrap";
+import { Nav, Navbar, Container } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 function Header() {
-      return (
-            <header>
-                  <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
-                        <Container>
-                              <Navbar.Brand href="/">
-								AnnaShop
-                              </Navbar.Brand>
-                              <Navbar.Toggle aria-controls="navbarScroll" />
-                              <Navbar.Collapse id="navbarScroll">
-                                    <Nav className="mr-auto">
-                                          <Nav.Link href="/cart">
-											<i className="fas fa-shopping-cart"></i>
-                                                Cart
-                                          </Nav.Link>
-                                          <Nav.Link href="/login">
-											<i className="fas fa-user"></i>
-                                                Login
-                                          </Nav.Link>
-                                    </Nav>
-                              </Navbar.Collapse>
-                        </Container>
-                  </Navbar>
-            </header>
-      );
+    return (
+          <header>
+                <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+                      <Container>
+                            <LinkContainer to="/">
+                                  <Navbar.Brand> AnnaShop </Navbar.Brand>
+                            </LinkContainer>
+                            <Navbar.Toggle aria-controls="navbarScroll" />
+                            <Navbar.Collapse id="navbarScroll">
+                                  <Nav className="mr-auto">
+                                        <LinkContainer to="/">
+                                              <Nav.Link>
+                                                    {" "}
+                                                    <i className="fas fa-home">
+                                                          {" "}
+                                                    </i>
+                                                    Home{" "}
+                                              </Nav.Link>
+                                        </LinkContainer>
+                                        <LinkContainer to="/cart">
+                                              <Nav.Link>
+                                                    <i className="fas fa-shopping-cart">
+                                                          {" "}
+                                                    </i>{" "}
+                                                    Cart{" "}
+                                              </Nav.Link>
+                                        </LinkContainer>
+                                        <LinkContainer to="/login">
+                                              <Nav.Link>
+                                                    <i className="fas fa-user">
+                                                          {" "}
+                                                    </i>{" "}
+                                                    Login{" "}
+                                              </Nav.Link>
+                                        </LinkContainer>
+                                  </Nav>{" "}
+                            </Navbar.Collapse>{" "}
+                      </Container>{" "}
+                </Navbar>{" "}
+          </header>
+    );
 }
 
 export default Header;
