a9082cda7f7d647b9e8cbd54ca55a1f069f4e951
Shindano Singi <shindanosingi@sekur.com>
2023-10-21 01:41:54 -0400

    changes files

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-// import { Container } from 'react-bootstrap'
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import Header from "./components/Header";
-// import Footer from "./components/Footer";
+import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-// import HomeScreen from "./screens/HomeScreen";
-// import ProductScreen from './screens/ProductScreen';
-// import CartScreen from "./screens/CartScreen";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from "./screens/CartScreen";
 
-// function App() {
-//       return (
-//             <Router>
-//                   <Header />
-//                   <main className='py-3'>
-//                         <Container>
-//                               <Routes>
-//                                     <Route path="/" element={<HomeScreen />} />
-//                                     <Route path="/product/:id" element={<ProductScreen/>} />
-//                                     <Route path="/cart/:id?" element={<CartScreen/>} />
-//                               </Routes>
-//                         </Container>
-//                   </main>
-//                   <Footer />
-//             </Router>
-//       );
-// }
+function App() {
+      return (
+            <Router>
+                  <Header />
+                  <main className='py-3'>
+                        <Container>
+                              <Routes>
+                                    <Route path="/" element={<HomeScreen />} />
+                                    <Route path="/product/:id" element={<ProductScreen/>} />
+                                    <Route path="/cart/:id?" element={<CartScreen/>} />
+                              </Routes>
+                        </Container>
+                  </main>
+                  <Footer />
+            </Router>
+      );
+}
 
-// export default App;
+export default App;
